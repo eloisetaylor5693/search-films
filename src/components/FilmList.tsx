859c313811682e63ee464ae45547cce50bf5ba53
eloisetaylor5693 <eloisetaylor5693@gmail.com>
2022-07-15 18:03:45 +0100

    Showing film images

--- src/components/FilmList.tsx
+++ src/components/FilmList.tsx
@@ -1,16 +1,18 @@
 import React from "react";
 import { FilmResultsContext } from "../templates/Search";
 import { Film } from "../Film";
+import "./FilmList.css";
 
 function FilmList(): JSX.Element {
   const contextData = React.useContext(FilmResultsContext);
 
   return (
-    <div>
+    <div className="film-list">
       {contextData.map((film: Film) => {
         return (
           <div key={film.id}>
-            <h2>{film.title}</h2>
+
+            <img src={film.backdrop} alt={`${film.title} poster`} height="300px"/>
           </div>
         );
       })}
