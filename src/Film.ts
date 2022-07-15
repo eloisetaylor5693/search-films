0b572a2164993c3b35aa5a103ef5cda31e274017
eloisetaylor5693 <eloisetaylor5693@gmail.com>
2022-07-15 17:22:11 +0100

    added fields needed for screens

--- src/Film.ts
+++ src/Film.ts
@@ -1,4 +1,14 @@
 export interface Film{
     id: string;
     title: string;
+    backdrop: string;
+    cast: string[];
+    genres: string[];
+    director: string;
+    imdb_rating: number;
+    length: string;
+    released_on: Date;
+    overview: string;
+    poster: string;
+    slug: string;
 }
