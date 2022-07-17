03efb50d502c5cd7e1612bf2d655dc8536d4c195
eloisetaylor5693 <eloisetaylor5693@gmail.com>
2022-07-17 14:24:30 +0100

    fixed tests after routing broke them

--- src/components/FilmGenreSection.spec.tsx
+++ src/components/FilmGenreSection.spec.tsx
@@ -2,6 +2,7 @@ import { Film } from "../Film";
 import "./FilmGenreSection.css";
 import { render, screen } from "@testing-library/react";
 import FilmGenreSection from "./FilmGenreSection";
+import { BrowserRouter } from "react-router-dom";
 
 const mockFilms: Film[] = [
   {
@@ -21,32 +22,39 @@ const mockFilms: Film[] = [
 ];
 
 describe("FilmGenreSection component", () => {
-  describe('given a list of films which match the given genre', () => {
+  describe("given a list of films which match the given genre", () => {
     it("Loads a genre title", () => {
-      render(<FilmGenreSection filmList={mockFilms} genre={"action"} />);
+      render(<FilmGenreSection filmList={mockFilms} genre={"action"} />, {
+        wrapper: BrowserRouter,
+      });
       const result = screen.getByText("action");
       expect(result).toBeDefined();
     });
-  
-    it("Loads a spacific film thumbnail", () => {
-      render(<FilmGenreSection filmList={mockFilms} genre={"action"} />);
+
+    it("Loads a specific film thumbnail", () => {
+      render(<FilmGenreSection filmList={mockFilms} genre={"action"} />, {
+        wrapper: BrowserRouter,
+      });
       const result = screen.getByAltText("hello poster");
       expect(result).toBeDefined();
     });
   });
 
-  describe('given a list of films which do not match the given genre', () => {
+  describe("given a list of films which do not match the given genre", () => {
     it("Loads a genre title", () => {
-      render(<FilmGenreSection filmList={mockFilms} genre={"sahdbfghsdbg"} />);
+      render(<FilmGenreSection filmList={mockFilms} genre={"sahdbfghsdbg"} />, {
+        wrapper: BrowserRouter,
+      });
       const result = screen.getByText("sahdbfghsdbg");
       expect(result).toBeDefined();
     });
-  
+
     it("Loads a spacific film thumbnail", () => {
-      render(<FilmGenreSection filmList={mockFilms} genre={"ajdfbhjkabf"} />);
+      render(<FilmGenreSection filmList={mockFilms} genre={"ajdfbhjkabf"} />, {
+        wrapper: BrowserRouter,
+      });
       const result = screen.queryByAltText("hello poster");
       expect(result).not.toBeInTheDocument();
     });
   });
-  
 });
