e3c636ad6f46dd77edafb4e5339af9c068cf762b
eloisetaylor5693 <eloisetaylor5693@gmail.com>
2022-07-17 13:31:29 +0100

    testing film details template

---
+++ src/templates/FilmDetails.spec.tsx
@@ -0,0 +1,56 @@
+import { Film } from "../Film";
+import FilmDetails from "./FilmDetails";
+import { render, screen } from "@testing-library/react";
+
+const mockFilm: Film = {
+  id: "123",
+  title: "hello",
+  backdrop: "url.com",
+  cast: ["Eloise", "My cat"],
+  genres: ["action", "drama"],
+  director: "Eloise",
+  imdb_rating: 4.9,
+  length: "2 hours",
+  released_on: new Date(2022, 5, 1),
+  overview: "it's a nice film",
+  poster: "url.com",
+  slug: "slug-film-title",
+};
+
+describe("FilmDetails template", () => {
+  it("Loads site title", () => {
+    render(<FilmDetails film={mockFilm} />);
+    const result = screen.getByText("WOOKIE MOVIES");
+    expect(result).toBeDefined();
+  });
+
+  it("should show film title", () => {
+    render(<FilmDetails film={mockFilm} />);
+    const result = screen.getByText(/hello/i);
+    expect(result).toBeDefined();
+  });
+
+  it("should show film rating", () => {
+    render(<FilmDetails film={mockFilm} />);
+    const result = screen.getByText(/(4.9)/i);
+    expect(result).toBeDefined();
+  });
+
+  it("should show film length", () => {
+    render(<FilmDetails film={mockFilm} />);
+    const result = screen.getByText(/2 hours/i);
+    expect(result).toBeDefined();
+  });
+
+  it("should show cast", () => {
+    render(<FilmDetails film={mockFilm} />);
+    const result = screen.getByText(/Eloise, My cat/i);
+    expect(result).toBeDefined();
+  });
+
+  it("should show description", () => {
+    render(<FilmDetails film={mockFilm} />);
+    const result = screen.getByText(/movie description: it's a nice film/i);
+    expect(result).toBeDefined();
+  });
+});
