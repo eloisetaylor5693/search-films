3c3df5a791894bd96bfb4806968347269ec4c352
eloisetaylor5693 <eloisetaylor5693@gmail.com>
2022-07-17 13:21:30 +0100

    Added header to the film details page

--- src/templates/FilmDetails.tsx
+++ src/templates/FilmDetails.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Header from "../components/Header";
 import { Film } from "../Film";
 import "./FilmDetails.css";
 
@@ -8,19 +9,23 @@ interface FilmDetailsProps {
 
 function FilmDetails({ film }: FilmDetailsProps): JSX.Element | null {
   if (!film) {
-    return null;
+    return <h2>Sorry, can't find  that film</h2>;
   }
 
   return (
-    <div className="details-container">
-      <img src={film.poster} alt={`${film.title} poster`} height="700px" />
-      <div className="film-description">
-        <h2>{`${film.title} (${film.imdb_rating})`}</h2>
-        <p>{`${film.released_on.getFullYear} | ${film.length} | ${film.director}`}</p>
-        <p>{`cast: ${film.cast.join(", ")}`}</p>
-        <p>{`Movie Description: ${film.overview}`}</p>
+    <>
+      <Header />
+
+      <div className="details-container">
+        <img src={film.poster} alt={`${film.title} poster`} height="700px" />
+        <div className="film-description">
+          <h2>{`${film.title} (${film.imdb_rating})`}</h2>
+          <p>{`${film.released_on.getFullYear} | ${film.length} | ${film.director}`}</p>
+          <p>{`cast: ${film.cast.join(", ")}`}</p>
+          <p>{`Movie Description: ${film.overview}`}</p>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
