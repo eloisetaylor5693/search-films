720cb68d8ad593057a2f7ba0ddafd6cefb7962c2
eloisetaylor5693 <eloisetaylor5693@gmail.com>
2022-07-18 09:47:36 +0100

    spacing

--- src/helpers/MapGenresFromFilmList.ts
+++ src/helpers/MapGenresFromFilmList.ts
@@ -3,9 +3,11 @@ import { Film } from "../Film";
 /** Returns a distinct list of all genres found in the film list provided */
 export const MapGenresFromFilmList = (films: Film[]): string[] => {
     const allGenres: string[] = [];
+
     films.forEach((x: Film) =>
       x.genres.forEach((y: string) => allGenres.push(y))
     );
+    
     const distinctGenres = Array.from(new Set(allGenres)).sort();
     return distinctGenres;
   };
